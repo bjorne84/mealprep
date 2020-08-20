5877c1f49baa03efcf61601aaff2ac32021ae37c
bjorne84 <edinbjorn@gmail.com>
2020-08-20 12:35:26 +0200

    la till password upprepmning html

--- skapakonto.php
+++ skapakonto.php
@@ -26,6 +26,8 @@ include('includes/header.php');
                         <input type="text" name="userName" id="userName" class="input" required><br>
                         <label for="password">* Välj ett lösenord (minst 6 tecken långt):</label><br>
                         <input type="text" name="password" id="password" class="input" required><br>
+                        <label for="passwordRepeat">* Upprepa lösenord:</label><br>
+                        <input type="text" name="passwordRepeat" id="password" class="input" required><br>
                         <input type="checkbox" name="checkbox" required><label for="checkbox">
                             Jag godkänner att ovanstående uppgifter lagras i syfte för
                             inloggning.</label><br>
