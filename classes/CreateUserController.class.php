a3e8bf4cb96a37ac74d5ecf574446d887cae22d0
bjorne84 <edinbjorn@gmail.com>
2020-08-20 17:04:34 +0200

    lagt till request-check i skapakonto.php och skapat createusercontroll metod i cuc.class.php (förkort)

--- classes/CreateUserController.class.php
+++ classes/CreateUserController.class.php
@@ -1,5 +1,20 @@
 <?php
 
 class CreateUserController extends UserModel {
-    
+
+
+    public function registerUser() {
+        $data = [
+            'foreName' => '',
+            'surName' => '',
+            'email' => '',
+            'userName' => '',
+            'password' => '',
+            'passwordRepeat' => ''
+        ];
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        $cleandata = $_POST;
+        return var_dump($cleandata);
+    }
+
 }
--- skapakonto.php
+++ skapakonto.php
@@ -2,6 +2,13 @@
 $page_title = "skapa konto på mealprep";
 include('includes/header.php');
 ?>
+    <?php
+    // check if form has ben sent and then start validate data
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $newUser = new CreateUserController;
+        $newUser->registerUser();
+    }
+?>
 <div class="mainWrapp">
     <div class="showBlogs">
         <div class="white-back">
@@ -17,7 +24,7 @@ include('includes/header.php');
                         <input type="text" name="foreName" id="foreName" class="input">
                         <br>
                         <label for="surname">Efternamn:</label><br>
-                        <input type="text" name="surename" id="surename" class="input">
+                        <input type="text" name="surName" id="surename" class="input">
                         <br>
                         <label for="email">* E-postadress:</label><br>
                         <input type="email" name="email" id="email" class="input" required>
@@ -25,9 +32,9 @@ include('includes/header.php');
                         <label for="userName">* Välj ett användarnamn/alias:</label><br>
                         <input type="text" name="userName" id="userName" class="input" required><br>
                         <label for="password">* Välj ett lösenord (minst 6 tecken långt):</label><br>
-                        <input type="text" name="password" id="password" class="input" required><br>
+                        <input type="password" name="password" id="password" class="input" required><br>
                         <label for="passwordRepeat">* Upprepa lösenord:</label><br>
-                        <input type="text" name="passwordRepeat" id="passwordRepeat" class="input" required><br>
+                        <input type="password" name="passwordRepeat" id="passwordRepeat" class="input" required><br>
                         <input type="checkbox" name="checkbox" required><label for="checkbox">
                             Jag godkänner att ovanstående uppgifter lagras i syfte för
                             inloggning.</label><br>
@@ -41,7 +48,9 @@ include('includes/header.php');
     <?php
     include('includes/aside.php');
     ?>
+
 </div>
+
 <?php
 include('includes/footer.php');
 ?>
